8ea4190659b54125f278af23cba7b9228cc9fc81
Dylar <kolja-bruns@gmx.de>
2016-05-09 23:46:47 +0200

    Initial commit

---
+++ android/src/de/dylar/spacerace/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.dylar.spacerace;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.dylar.spacerace.SpaceRaceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceRaceGame(), config);
+	}
+}
---
+++ desktop/src/de/dylar/spacerace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.dylar.spacerace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.dylar.spacerace.SpaceRaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceRaceGame(), config);
+	}
+}
